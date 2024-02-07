8063593d5d9cd22887f9bfb90a1a1b3b2bfc8b9e
Jules <ju.pabst@gmail.com>
2024-02-07 16:11:26 +0100

    delete data file and add Category and Item files instead

--- ToDo/ToDo/Model/Category.swift
+++ ToDo/ToDo/Model/Category.swift
@@ -1,9 +1,8 @@
 //
-//  File.swift
+//  Category.swift
 //  ToDo
 //
 //  Created by Julia Pabst on 07.02.24.
 //
 
 import Foundation
-import RealmSwift 
---
+++ ToDo/ToDo/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Julia Pabst on 07.02.24.
+//
+
+import Foundation
