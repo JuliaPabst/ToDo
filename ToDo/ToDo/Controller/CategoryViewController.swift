3a40df1921b3f400164f2edccf140aa4e49e47bd
Jules <ju.pabst@gmail.com>
2024-02-07 16:47:41 +0100

    prevent app from crashing by not force unwrapping categories when loading them

--- ToDo/ToDo/Controller/CategoryViewController.swift
+++ ToDo/ToDo/Controller/CategoryViewController.swift
@@ -12,7 +12,7 @@ class CategoryViewController: UITableViewController {
     
     let realm = try! Realm()
     
-    var categories: Results<Category>!
+    var categories: Results<Category>?
    
 
     override func viewDidLoad() {
@@ -23,12 +23,12 @@ class CategoryViewController: UITableViewController {
     
     //MARK: - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categories[indexPath.row].name
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
         
         return cell
     }
@@ -98,7 +98,7 @@ class CategoryViewController: UITableViewController {
         let destinationVC = segue.destination as! ToDoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
     }
     
