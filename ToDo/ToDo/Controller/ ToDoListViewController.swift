0634baa9a682a7b48cb7337586d45cc3cc8001d3
Jules <ju.pabst@gmail.com>
2024-02-07 16:59:20 +0100

    change itemsArray to items everywhere in ToDoViewcontroller and adapt types

--- ToDo/ToDo/Controller/ ToDoListViewController.swift
+++ ToDo/ToDo/Controller/ ToDoListViewController.swift
@@ -34,24 +34,25 @@ class ToDoListViewController: UITableViewController {
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return items?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        if let item = items?[indexPath.row]{
+            cell.textLabel?.text = item.title
+            
+            cell.accessoryType = item.done ?  .checkmark : .none
+        } else {
+            cell.textLabel?.text = "No items added"
+        }
         
-        cell.accessoryType = itemArray[indexPath.row].done ?  .checkmark : .none
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        /* context.delete(itemArray[indexPath.row])
-        itemArray.remove(at: indexPath.row) */
-        
-       itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+       // items[indexPath.row].done = !items[indexPath.row].done
         
         self.saveItems()
         
