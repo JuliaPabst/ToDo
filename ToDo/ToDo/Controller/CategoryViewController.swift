d818ff4563e7f2da472fd565f82b0b3c0d82818c
Jules <ju.pabst@gmail.com>
2024-02-09 14:47:42 +0100

    copy code from SwipeTableView extension in categoryViewController to SwipeTableViewController

--- ToDo/ToDo/Controller/CategoryViewController.swift
+++ ToDo/ToDo/Controller/CategoryViewController.swift
@@ -109,32 +109,6 @@ class CategoryViewController: UITableViewController {
 
 //MARK: - Swipe Cell Delegate Methods
 extension CategoryViewController: SwipeTableViewCellDelegate {
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-        
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            if let currentCategory = self.categories?[indexPath.row]{
-                do {
-                    try self.realm.write {
-                        self.realm.delete(currentCategory)
-                    }
-                } catch {
-                    print("Cannot delete  item checked \(error)")
-                }
-            }
-        }
-        
-        // customize the action appearance
-        deleteAction.image = UIImage(named: "delete-icon")
-        
-        return [deleteAction]
-    }
-    
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        return options
-    }
+
     
 }
